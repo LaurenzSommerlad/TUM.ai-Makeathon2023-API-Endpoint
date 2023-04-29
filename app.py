c069cde7356c6d08bdb81a24696975bf29c066e2
Laurenz Sommerlad <36761347+LaurenzSommerlad@users.noreply.github.com>
2023-04-29 14:38:46 +0200

    Revert "Bug/fix internal eroor"

--- app.py
+++ app.py
@@ -1,31 +1,17 @@
 from fastapi import FastAPI
 from pydantic import BaseModel, Json, ValidationError
-from typing import Any, Optional, List
+from typing import Any
 from logic import build_question
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=['*'],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
 class AskItem(BaseModel):
-    context: Any
-
+    context: Json[Any]
 
 @app.post("/ask")
 async def askEndpoint(item: AskItem):
-    print(item)
     report, context = build_question(item.context)
-    print({"report": report, "context": context})
     return {"report": report, "context": context}
-
 
 @app.post("/chat")
 async def chatEndpoint(item: AskItem):
--- logic.py
+++ logic.py
@@ -1,12 +1,10 @@
 # implement the logic of the discussion
 def build_question(context):
-    print("inside logic", context)
     report = 0
-    context = list(context)
     id = len(context)
-    tmp = {"question": "this is a question"
-            , "response": "this is a response", "id": id}
-    context.append(tmp)
+    tmp =  {"question": "this is a question"
+            , "response": "this is a response"}
+    context.append({id: tmp})
     return report, context
 
 def chat():
